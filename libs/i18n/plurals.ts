ac3519cc5f07dbdb48ed04793029ef53a8393235
kirilldyachkovskiy <kirilldyachkovskiy@gmail.com>
2023-08-06 16:02:38 +0300

    init project

---
+++ libs/i18n/plurals.ts
@@ -0,0 +1,7 @@
+export function createPluralize(locale: string) {
+  const rules = new Intl.PluralRules(locale);
+
+  return (count: number) => {
+    return rules.select(count);
+  };
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    eslint: {
+      dirs: ['libs'],
+    },
+}
 
 module.exports = nextConfig
